3516adeb832a7eca49293fc18ad9c7648b189dec
krasow <krasow@u.northwestern.edu>
2024-07-29 17:55:21 -0500

    writing to index space

--- dpu/dpu_test_realm.cc
+++ dpu/dpu_test_realm.cc
@@ -10,6 +10,7 @@ extern "C" {
 
 #include <realm/upmem/realm_c_upmem.h>
 
+volatile double ok = 0.0;
 
 typedef struct somethingelse {
     Rect<2> bounds;
@@ -21,7 +22,7 @@ typedef struct __DEVICE_DPU_LAUNCH_ARGS {
     // Rect<2> bounds;
     // AffineAccessor<double, 2> linear_accessor;
     // PADDING(16);
-    char paddd[48];
+    char paddd[64];
 } __attribute__((aligned(8))) __DEVICE_DPU_LAUNCH_ARGS;
 
 __host __DEVICE_DPU_LAUNCH_ARGS ARGS;  
@@ -43,34 +44,25 @@ int main_kernel1() {
     
     printf("test->linear_accessor.strides[0]: %lu\n", test->linear_accessor.strides[0]);
     printf("test->linear_accessor.strides[1]: %lu\n", test->linear_accessor.strides[1]);
-
-
-    /*
-    somethingelse test; 
-
-    printf("sizeof(somethingelse): %d\n", sizeof(somethingelse));
-    printf("offset of bounds: %lu\n", offsetof(somethingelse, bounds));
-    printf("offset of linear_accessor: %lu\n", offsetof(somethingelse, linear_accessor));
-    printf("sizeof(test.linear_accessor): %d\n", sizeof(test.linear_accessor));
-    */
     
-    // __DEVICE_DPU_LAUNCH_ARGS *ARGS = (__DEVICE_DPU_LAUNCH_ARGS *)mem_alloc(sizeof(__DEVICE_DPU_LAUNCH_ARGS));
+    unsigned int i, j;
+    for (i = 0; i < 100; i++) {
+        for (j = 0; j < 100; j++) {
+            ok = test->linear_accessor[Point<2>(i, j)];
+        }
+    }  
 
-    // mram_read((__mram_ptr void const*)(DPU_MRAM_HEAP_POINTER), (void*)ARGS, sizeof(__DEVICE_DPU_LAUNCH_ARGS));
-    
-    // double value = test->linear_accessor[Point<2>(0, 0)];
-    
-    // Point<2> psomethign(33, 1);
-    // assert((uint64_t)test != 0 && ((uint64_t)test % 8) == 0);
+    printf("ok is %1.3f at (%d, %d)\n", ok, 99, 99);
 
-    printf("%lf \n", test->linear_accessor[Point<2>(33, 63)]);
 
-    // for (unsigned int i = 0; i < 2; i++) {
-    //     for (unsigned int j = 0; j < 20; j++) {
-    //         printf("%f ", test->linear_accessor[Point<2>(i, j)]);
-    //     }
-    // }  
+    for (i = 0; i < 100; i++) {
+        for (j = 0; j < 100; j++) {
+            test->linear_accessor.write(Point<2>(i, j), 3.32);
+        }
+    }  
 
+    ok = test->linear_accessor[Point<2>(99, 99)];
+    printf("ok is %1.3f at (%d, %d) after write\n", ok, 99, 99);
 
     return 0;
 }
