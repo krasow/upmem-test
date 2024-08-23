9ff6be61707b4b9787af17c223a3216be85ad0e2
zzhyyds <zhehaozhu111@gamil.com>
2024-08-22 20:24:50 -0500

    modify the test script

--- test.sh
+++ test.sh
@@ -1,7 +1,17 @@
 # make clean
 nice make -j 100
 
-./upmem_test -ll:num_dpus 1
+for i in {1..10}
+do
+  ./upmem_test -ll:num_dpus 1
+
+  if [ $? -ne 0 ]; then
+    echo "Error occurred at iteration $i. Exiting loop."
+    break
+  fi
+done
+
 
 python visual.py -d uint32 -i host_array.bin -o host_array.png -r 32 -c 32
 python visual.py -d uint32 -i array.bin      -o device_array.png -r 32 -c 32
+diff host_array.png device_array.png
