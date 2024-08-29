63b30bf7479260a72fdd3ff338cd316fa58c845b
krasow <krasow@u.northwestern.edu>
2024-08-29 10:43:35 -0500

    clang format

--- dpu/dpu_test_realm.cc
+++ dpu/dpu_test_realm.cc
@@ -51,19 +51,18 @@ int main_kernel1() {
 
 #ifdef PRINT_UPMEM
   if (tasklet_id == 0) {
-  printf("DEVICE:::: Running daxpy computation with xptr %p, y_ptr %p, z_ptr "
-         "%p...",
-         args->acc_x.ptr(args->rect.lo), args->acc_y.ptr(args->rect.lo),
-         args->acc_z.ptr(args->rect.lo));
+    printf("DEVICE:::: Running daxpy computation with xptr %p, y_ptr %p, z_ptr "
+           "%p...",
+           args->acc_x.ptr(args->rect.lo), args->acc_y.ptr(args->rect.lo),
+           args->acc_z.ptr(args->rect.lo));
 
 #ifdef INT32
-  printf(" alpha = %d \n", args->alpha);
+    printf(" alpha = %d \n", args->alpha);
 #elif DOUBLE
-  printf(" alpha = %f \n", args->alpha);
+    printf(" alpha = %f \n", args->alpha);
 #endif
   }
-#endif 
-
+#endif
 
   Rect<1> rect;
   rect.lo = args->rect.lo + tasklet_id * BLOCK_SIZE;
@@ -82,7 +81,7 @@ int main_kernel1() {
   block_acc_y.accessor.strides = args->acc_y.accessor.strides;
   block_acc_z.accessor.strides = args->acc_z.accessor.strides;
 
-  // iterator through all elements 
+  // iterator through all elements
   for (Legion::PointInRectIterator<1> pir(rect); pir();
        pir += (NR_TASKLETS * BLOCK_SIZE)) {
 
@@ -97,14 +96,15 @@ int main_kernel1() {
     block_rect.hi = BLOCK_SIZE;
 
     // block iterator
-    for (Legion::PointInRectIterator<1> pir_block(block_rect); pir_block(); pir_block++) {
+    for (Legion::PointInRectIterator<1> pir_block(block_rect); pir_block();
+         pir_block++) {
       block_acc_z.write(*pir_block, args->alpha * block_acc_x[*pir_block] +
                                         block_acc_y[*pir_block]);
     }
 
-    // write block 
+    // write block
     // #define WRITE_BLOCK(point, acc_full, acc_block, bytes)
     WRITE_BLOCK(*pir, args->acc_z, block_acc_z, BLOCK_SIZE * sizeof(TYPE));
-  } 
+  }
   return 0;
 }
