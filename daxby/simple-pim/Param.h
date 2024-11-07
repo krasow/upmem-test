bd0e3e7e260131ece851c60336b72bb66cccf3f7
krasow <krasow@u.northwestern.edu>
2024-11-06 23:57:59 -0600

    script download fix

--- daxby/simple-pim/Param.h
+++ daxby/simple-pim/Param.h
@@ -2,8 +2,8 @@
 #define PARAM_H
 #include <stdlib.h>
 
-typedef uint32_t T; 
+typedef double T; 
 const uint32_t dpu_number = 32; //2432
 uint32_t print_info = 0;
-uint64_t nr_elements = dpu_number*128000; //dpu_number*1000000
+uint64_t nr_elements = dpu_number*131072; //dpu_number*1000000
 #endif
