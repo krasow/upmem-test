aa0ac312dae62719d78a689a08a816030b7b9bfb
zzhyyds <zhehaozhu111@gamil.com>
2024-12-05 13:23:02 -0600

    add the origin benchmark

--- prim_suite/legion-pim/HST_S/host/host_hst.cc
+++ prim_suite/legion-pim/HST_S/host/host_hst.cc
@@ -14,6 +14,10 @@
  * limitations under the License.
  */
 
+//TODO: the exp mod
+//TODO: do we need the dynamic range handling?
+
+
 #include <cassert>
 #include <cstdio>
 #include <cstdlib>
@@ -21,9 +25,7 @@
 #include <math.h>
 #include <sys/time.h>
 
-/* legion programming system library */
 #include <legion.h>
-/* common header between device and host */
 #include <common.h>
 
 #if !defined(LEGION_USE_UPMEM)
@@ -55,7 +57,6 @@ typedef struct {
   Realm::Upmem::Kernel *kernel;
 } DPU_TASK_ARGS;
 
-// for the device
 #define DPU_LAUNCH_BINARY "dpu/dpu_test_realm.up.o"
 
 enum FieldIDs {
@@ -91,15 +92,11 @@ void top_level_task(const Task *task,
                     const std::vector<PhysicalRegion> &regions, Context ctx,
                     Runtime *runtime) {
   Realm::Upmem::Kernel *kern = new Realm::Upmem::Kernel(DPU_LAUNCH_BINARY);
-  // the binary needs to be loaded before any memory operations
   kern->load();
 
   int num_elements = 256;
   int num_subregions = 32;
   int soa_flag = 0;
-  // See if we have any command line arguments to parse
-  // Note we now have a new command line parameter which specifies
-  // how many subregions we should make.
   {
     const InputArgs &command_args = Runtime::get_input_args();
     for (int i = 1; i < command_args.argc; i++) {
@@ -114,7 +111,6 @@ void top_level_task(const Task *task,
   printf("Running daxpy for %d elements...\n", num_elements);
   printf("Partitioning data into %d sub-regions...\n", num_subregions);
 
-  // Create our logical regions using the same schemas as earlier examples
   Rect<1> elem_rect(0, num_elements - 1);
   IndexSpace is = runtime->create_index_space(ctx, elem_rect);
   runtime->attach_name(is, "is");
@@ -175,136 +171,62 @@ void top_level_task(const Task *task,
       launcher.external_resource = &resource;
       z_pr = runtime->attach_external_resource(ctx, launcher);
     }
-  } else { // AOS
-    size_t total_bytes = sizeof(daxpy_t) * (num_elements);
-    daxpy_t *xyz_ptr = (daxpy_t *)malloc(total_bytes);
-    std::vector<FieldID> layout_constraint_fields(3);
-    layout_constraint_fields[0] = FID_X;
-    layout_constraint_fields[1] = FID_Y;
-    layout_constraint_fields[2] = FID_Z;
-    // Need separate attaches for different logical regions,
-    // each launcher gets all the fields in the layout constraint
-    // but only requests privileges on fields for its logical region
-    printf("Attach AOS array ptr %p\n", xyz_ptr);
-    {
-      AttachLauncher launcher(LEGION_EXTERNAL_INSTANCE, input_lr, input_lr);
-      launcher.initialize_constraints(false /*column major*/, false /*soa*/,
-                                      layout_constraint_fields);
-      launcher.privilege_fields.insert(FID_X);
-      launcher.privilege_fields.insert(FID_Y);
-      Realm::ExternalMemoryResource resource(xyz_ptr, total_bytes);
-      launcher.external_resource = &resource;
-      xy_pr = runtime->attach_external_resource(ctx, launcher);
-    }
-    {
-      AttachLauncher launcher(LEGION_EXTERNAL_INSTANCE, output_lr, output_lr);
-      launcher.initialize_constraints(false /*columns major*/, false /*soa*/,
-                                      layout_constraint_fields);
-      launcher.privilege_fields.insert(FID_Z);
-      Realm::ExternalMemoryResource resource(xyz_ptr, total_bytes);
-      launcher.external_resource = &resource;
-      z_pr = runtime->attach_external_resource(ctx, launcher);
-    }
-  }
+  } 
+  // else { // AOS
+  //   size_t total_bytes = sizeof(daxpy_t) * (num_elements);
+  //   daxpy_t *xyz_ptr = (daxpy_t *)malloc(total_bytes);
+  //   std::vector<FieldID> layout_constraint_fields(3);
+  //   layout_constraint_fields[0] = FID_X;
+  //   layout_constraint_fields[1] = FID_Y;
+  //   layout_constraint_fields[2] = FID_Z;
+  //   // Need separate attaches for different logical regions,
+  //   // each launcher gets all the fields in the layout constraint
+  //   // but only requests privileges on fields for its logical region
+  //   printf("Attach AOS array ptr %p\n", xyz_ptr);
+  //   {
+  //     AttachLauncher launcher(LEGION_EXTERNAL_INSTANCE, input_lr, input_lr);
+  //     launcher.initialize_constraints(false /*column major*/, false /*soa*/,
+  //                                     layout_constraint_fields);
+  //     launcher.privilege_fields.insert(FID_X);
+  //     launcher.privilege_fields.insert(FID_Y);
+  //     Realm::ExternalMemoryResource resource(xyz_ptr, total_bytes);
+  //     launcher.external_resource = &resource;
+  //     xy_pr = runtime->attach_external_resource(ctx, launcher);
+  //   }
+  //   {
+  //     AttachLauncher launcher(LEGION_EXTERNAL_INSTANCE, output_lr, output_lr);
+  //     launcher.initialize_constraints(false /*columns major*/, false /*soa*/,
+  //                                     layout_constraint_fields);
+  //     launcher.privilege_fields.insert(FID_Z);
+  //     Realm::ExternalMemoryResource resource(xyz_ptr, total_bytes);
+  //     launcher.external_resource = &resource;
+  //     z_pr = runtime->attach_external_resource(ctx, launcher);
+  //   }
+  // }
 
-  // In addition to using rectangles and domains for launching index spaces
-  // of tasks (see example 02), Legion also uses them for performing
-  // operations on logical regions.  Here we create a rectangle and a
-  // corresponding domain for describing the space of subregions that we
-  // want to create.  Each subregion is assigned a 'color' which is why
-  // we name the variables 'color_bounds' and 'color_domain'.  We'll use
-  // these below when we partition the region.
   Rect<1> color_bounds(0, num_subregions - 1);
   IndexSpace color_is = runtime->create_index_space(ctx, color_bounds);
 
-  // Parallelism in Legion is implicit.  This means that rather than
-  // explicitly saying what should run in parallel, Legion applications
-  // partition up data and tasks specify which regions they access.
-  // The Legion runtime computes non-interference as a function of
-  // regions, fields, and privileges and then determines which tasks
-  // are safe to run in parallel.
-  //
-  // Data partitioning is performed on index spaces.  The partitioning
-  // operation is used to break an index space of points into subsets
-  // of points each of which will become a sub index space.  Partitions
-  // created on an index space are then transitively applied to all the
-  // logical regions created using the index space.  We will show how
-  // to get names to the subregions later in this example.
-  //
-  // Here we want to create the IndexPartition 'ip'.  We'll illustrate
-  // two ways of creating an index partition depending on whether the
-  // array being partitioned can be evenly partitioned into subsets
-  // or not.  There are other methods to partitioning index spaces
-  // which are not covered here.  We'll cover the case of coloring
-  // individual points in an index space in our capstone circuit example.
   IndexPartition ip = runtime->create_equal_partition(ctx, is, color_is);
   runtime->attach_name(ip, "ip");
 
-  // The index space 'is' was used in creating two logical regions: 'input_lr'
-  // and 'output_lr'.  By creating an IndexPartitiong of 'is' we implicitly
-  // created a LogicalPartition for each of the logical regions created using
-  // 'is'.  The Legion runtime provides several ways of getting the names for
-  // these LogicalPartitions.  We'll look at one of them here.  The
-  // 'get_logical_partition' method takes a LogicalRegion and an IndexPartition
-  // and returns the LogicalPartition of the given LogicalRegion that
-  // corresponds to the given IndexPartition.
   LogicalPartition input_lp = runtime->get_logical_partition(ctx, input_lr, ip);
   runtime->attach_name(input_lp, "input_lp");
   LogicalPartition output_lp =
       runtime->get_logical_partition(ctx, output_lr, ip);
   runtime->attach_name(output_lp, "output_lp");
 
-  // Create our launch domain.  Note that is the same as color domain
-  // as we are going to launch one task for each subregion we created.
   ArgumentMap arg_map;
   double start_init = get_cur_time();
 
-  // As in previous examples, we now want to launch tasks for initializing
-  // both the fields.  However, to increase the amount of parallelism
-  // exposed to the runtime we will launch separate sub-tasks for each of
-  // the logical subregions created by our partitioning.  To express this
-  // we create an IndexLauncher for launching an index space of tasks
-  // the same as example 02.
   IndexLauncher init_launcher(INIT_FIELD_TASK_ID, color_is,
                               TaskArgument(NULL, 0), arg_map);
 
-  // For index space task launches we don't want to have to explicitly
-  // enumerate separate region requirements for all points in our launch
-  // domain.  Instead Legion allows applications to place an upper bound
-  // on privileges required by subtasks and then specify which privileges
-  // each subtask receives using a projection function.  In the case of
-  // the field initialization task, we say that all the subtasks will be
-  // using some subregion of the LogicalPartition 'input_lp'.  Applications
-  // may also specify upper bounds using logical regions and not partitions.
-  //
-  // The Legion implementation assumes that all all points in an index
-  // space task launch request non-interfering privileges and for performance
-  // reasons this is unchecked.  This means if two tasks in the same index
-  // space are accessing aliased data, then they must either both be
-  // with read-only or reduce privileges.
-  //
-  // When the runtime enumerates the launch_domain, it will invoke the
-  // projection function for each point in the space and use the resulting
-  // LogicalRegion computed for each point in the index space of tasks.
-  // The projection ID '0' is reserved and corresponds to the identity
-  // function which simply zips the space of tasks with the space of
-  // subregions in the partition.  Applications can register their own
-  // projections functions via the 'register_region_projection' and
-  // 'register_partition_projection' functions before starting
-  // the runtime similar to how tasks are registered.
   init_launcher.add_region_requirement(RegionRequirement(
       input_lp, 0 /*projection ID*/, WRITE_DISCARD, EXCLUSIVE, input_lr));
   init_launcher.region_requirements[0].add_field(FID_X);
   FutureMap fmi0 = runtime->execute_index_space(ctx, init_launcher);
 
-  // Modify our region requirement to initialize the other field
-  // in the same way.  Note that after we do this we have exposed
-  // 2*num_subregions task-level parallelism to the runtime because
-  // we have launched tasks that are both data-parallel on
-  // sub-regions and task-parallel on accessing different fields.
-  // The power of Legion is that it allows programmers to express
-  // these data usage patterns and automatically extracts both
-  // kinds of parallelism in a unified programming framework.
   init_launcher.region_requirements[0].privilege_fields.clear();
   init_launcher.region_requirements[0].instance_fields.clear();
   init_launcher.region_requirements[0].add_field(FID_Y);
@@ -320,10 +242,7 @@ void top_level_task(const Task *task,
   DPU_TASK_ARGS args;
   args.alpha = alpha;
   args.kernel = kern;
-  // We launch the subtasks for performing the daxpy computation
-  // in a similar way to the initialize field tasks.  Note we
-  // again make use of two RegionRequirements which use a
-  // partition as the upper bound for the privileges for the task.
+
   IndexLauncher daxpy_launcher(DAXPY_TASK_ID, color_is,
                                TaskArgument(&args, sizeof(DPU_TASK_ARGS)),
                                arg_map);
@@ -339,14 +258,6 @@ void top_level_task(const Task *task,
   double end_t = get_cur_time();
   printf("Attach array, daxpy done, time %f\n", end_t - start_t);
 
-  // While we could also issue parallel subtasks for the checking
-  // task, we only issue a single task launch to illustrate an
-  // important Legion concept.  Note the checking task operates
-  // on the entire 'input_lr' and 'output_lr' regions and not
-  // on the subregions.  Even though the previous tasks were
-  // all operating on subregions, Legion will correctly compute
-  // data dependences on all the subtasks that generated the
-  // data in these two regions.
   TaskLauncher check_launcher(CHECK_TASK_ID,
                               TaskArgument(&alpha, sizeof(alpha)));
   check_launcher.add_region_requirement(
@@ -386,9 +297,6 @@ void init_field_task(const Task *task,
 
   const AccessorWD acc(regions[0], fid);
 
-  // Note here that we get the domain for the subregion for
-  // this task from the runtime which makes it safe for running
-  // both as a single task and as part of an index space of tasks.
   Rect<1> rect = runtime->get_index_space_domain(
       ctx, task->regions[0].region.get_index_space());
   for (PointInRectIterator<1> pir(rect); pir(); pir++)
@@ -455,9 +363,7 @@ void check_task(const Task *task, const std::vector<PhysicalRegion> &regions,
   for (PointInRectIterator<1> pir(rect); pir(); pir++) {
     TYPE expected = alpha * acc_x[*pir] + acc_y[*pir];
     TYPE received = acc_z[*pir];
-    // Probably shouldn't check for floating point equivalence but
-    // the order of operations are the same should they should
-    // be bitwise equal.
+
     if (!COMPARE(expected, received)) {
       all_passed = false;
       PRINT_EXPECTED(expected, received);
